c8630ad0b6dc0192856babea3e0b2b96eab38dc8
Divinariuz <b6099@oic.jp>
2017-11-10 12:12:03 +0900

    merge2

---
+++ database/migrations/2017_10_28_034933_create_problems_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProblemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('problems', function (Blueprint $table) {
+            $table->increments('id');  //問題ID
+            $table->string('question');  //問題
+            $table->string('answer1');
+            $table->string('answer2');
+            $table->string('answer3');
+            $table->string('answer4');
+            $table->string('explain'); //正解
+            $table->string('correct'); //解説
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('problems');
+    }
+}
