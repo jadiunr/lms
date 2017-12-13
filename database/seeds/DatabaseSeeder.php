b24784f8de6e11ed8d90096e033232d0cccb4c9f
sooooota <b6352@oic.jp>
2017-12-13 21:25:34 +0900

    add  call RecordsTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ProblemsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(ChangelogsTableSeeder::class);
+        $this->call(RecordsTableSeeder::class);
     }
 }
